90a14b74650042a8955c74772b7e9bd6618e162a
Vlgdev <vig-dev@mail.ru>
2020-09-27 19:56:41 +0300

    Auth

--- src/index.js
+++ src/index.js
@@ -1,51 +1,71 @@
-import Question from './question'
-import Modal from './modal'
-import { inputValid } from './utils'
-import './assets/css/style.css'
+import Question from './question';
+import Modal from './modal';
+import { inputValid } from './utils';
+import './assets/css/style.css';
 
-const askForm = document.getElementById('ask-form')
-const askInput = askForm.querySelector('#question')
-const askBtn = askForm.querySelector('.ask-form__btn')
-const btnModal = document.getElementById('btn-modal')
-const modal = document.getElementById('modal')
+const askForm = document.getElementById('ask-form');
+const askInput = askForm.querySelector('#question');
+const askBtn = askForm.querySelector('.ask-form__btn');
+const btnModal = document.getElementById('btn-modal');
+const modal = document.getElementById('modal');
 
-const modalInstance = M.Modal.init(modal)
-window.addEventListener('load', Question.render)
-askForm.addEventListener('submit', submitHandler)
-askInput.addEventListener('input', inputHandler)
-btnModal.addEventListener('click', openModal)
+const modalInstance = M.Modal.init(modal);
+window.addEventListener('load', Question.render);
+askForm.addEventListener('submit', submitHandler);
+askInput.addEventListener('input', inputHandler);
+btnModal.addEventListener('click', openModal);
 
 function inputHandler(event) {
-  askBtn.disabled = !inputValid(event.target.value)
+  askBtn.disabled = !inputValid(event.target.value);
 }
 
 function submitHandler(event) {
-  event.preventDefault()
+  event.preventDefault();
 
   if (inputValid(askInput.value)) {
-    askBtn.disabled = true
+    askBtn.disabled = true;
     Question.create({
       content: askInput.value,
       date: new Date().toJSON(),
     }).then((q) => {
-      askInput.value = ''
-    })
+      askInput.value = '';
+    });
   }
 }
 
 function openModal(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  modal.innerHTML = Modal.getHTML(Modal.getAuth())
+  modal.innerHTML = Modal.getHTML(Modal.getAuth());
 
-  modalInstance.open()
-  document
-    .getElementById('auth-form')
-    .addEventListener('submit', authHandler)
+  modalInstance.open();
+  document.getElementById('auth-form').addEventListener('submit', authHandler);
 }
 
 function authHandler(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  console.log(event.target.email.value, event.target.password.value);
+  const formData = {
+    email: event.target.email.value,
+    password: event.target.password.value,
+  };
+
+  const apiKey = process.env.JS_APP_API_KEY
+  fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({ ...formData, returnSecureToken: true }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -16,7 +17,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new Dotenv()
   ],
   module: {
     rules: [
