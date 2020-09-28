2d22f07a1c61c8b1d5cc9bb67a14ffc781fb8ed2
Vlgdev <vig-dev@mail.ru>
2020-09-28 20:01:31 +0300

    Auth and fetch all questions

--- src/question.js
+++ src/question.js
@@ -8,18 +8,39 @@ export default class Question {
       },
     })
       .then((res) => res.json())
-      .then((res) => ({...question, id: res,name}))
+      .then((res) => ({ ...question, id: res, name }))
       .then(addToLocaleStorage)
       .then(Question.render)
+  }
+
+  static fetchAll(token) {
+    if (!token) {
+      return Promise.resolve({
+        title: 'Ошибка',
+        content: `<p class="modal-error">У вас нет токена</p>`,
+      })
+    }
+
+    return fetch(
+      `https://native-app-c7210.firebaseio.com/questions.json?auth=${token}`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        data ? Object.keys(data).map((k) => ({ ...data[k], id: k })) : []
+      )
+      .then((questions) => ({
+        title: 'Список вопросов',
+        content: renderModalList(questions),
+      }))
   }
 
   static render() {
     const questions = getFormLocaleStorage()
     const box = document.getElementById('questions-box')
 
-    const html = questions.length 
-    ? questions.map(toCard).join('')
-    : `<p>Вы пока не задавали вопросов</p>`
+    const html = questions.length
+      ? questions.map(toCard).join('')
+      : `<p>Вы пока не задавали вопросов</p>`
 
     box.innerHTML = html
   }
@@ -37,14 +58,18 @@ function getFormLocaleStorage() {
 
 function toCard(q) {
   return `
-  <div class="questions__item">
-    <div class="questions__item-date">
+  <div class="question">
+    <div class="question-date">
       ${new Date(q.date).toLocaleDateString()} 
       ${new Date(q.date).toLocaleTimeString()}
     </div>
-    <div class="questions__item-body">
+    <div class="question-body">
       ${q.content}
     </div>
   </div>
   `
+}
+
+function renderModalList(questions) {
+  return questions.length ? questions.map(toCard).join('') : '<p>Вопросов пока нет</p>'
 }
